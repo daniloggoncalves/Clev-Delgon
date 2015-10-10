b999ee128d365c3a74fc261ea54b8c2fb3bf9b86
Danilo <danilo_kun@hotmail.com>
2015-10-10 21:53:44 +0900

    Direction
    Made the player object face the player's finger

--- Clev-Delgon/Assets/Scripts/PlayerScript.cs
+++ Clev-Delgon/Assets/Scripts/PlayerScript.cs
@@ -13,7 +13,22 @@ public class PlayerScript : MonoBehaviour {
 	
 	void FixedUpdate () {
         Camera_Controller();
+        PlayerDirection();
 
+    }
+
+
+    void PlayerDirection()
+    {
+            Touch touch_one = Input.GetTouch(0);
+            if (touch_one.phase != TouchPhase.Ended)
+            {
+                float angle = Mathf.Atan2(
+            transform.position.y - ((touch_one.position.x / (float)Screen.width) * 320.0f - 160.0f),
+            transform.position.x - ((touch_one.position.y / (float)Screen.height) * 200.0f - 100.0f)) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, 0, angle + 90);
+            }
+        
     }
 
     void Camera_Controller()
