1c1f9d23317fcd5182efe211f88ccf39edeea398
Danilo <danilo_kun@hotmail.com>
2015-10-10 21:36:40 +0900

    New Project
    Starting app from zero

---
+++ Clev-Delgon/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public GameObject BackGround;
+    public float Screen_Speed_y;
+    public float Screen_Speed_x;
+
+    void Start () {
+        transform.position = new Vector3(0, 0, 0);
+    }
+	
+	void FixedUpdate () {
+        Camera_Controller();
+
+    }
+
+    void Camera_Controller()
+    {
+        Touch touch_one = Input.GetTouch(0);
+        if (touch_one.phase != TouchPhase.Ended)
+        {
+            BackGround = UnityEngine.GameObject.FindGameObjectWithTag("BackGround");
+
+            Vector2 Direction = new Vector2(
+                ((touch_one.position.x / (float)Screen.width) * 320.0f - 160.0f),
+                ((touch_one.position.y / (float)Screen.height) * 200.0f - 100.0f));
+                
+            BackGround.GetComponent<Rigidbody2D>().velocity = (Direction/Direction.magnitude) * Screen_Speed_x;
+        }
+
+        else
+        {
+            BackGround = UnityEngine.GameObject.FindGameObjectWithTag("BackGround");
+            BackGround.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
+        }
+    }
+}
