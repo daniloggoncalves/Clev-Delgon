6d11728bcd6d4f48e347ae98f699f9048c1580cf
Danilo <danilo_kun@hotmail.com>
2015-10-11 04:43:33 +0900

    WereWolf AI
    werewolf follows closest villager

--- Clev-Delgon/Assets/Scripts/WereWolfNPCBehavior.cs
+++ Clev-Delgon/Assets/Scripts/WereWolfNPCBehavior.cs
@@ -3,11 +3,60 @@ using System.Collections;
 
 public class WereWolfNPCBehavior : MonoBehaviour {
 
-	void Start () {
-	
+    public float WereWolf_Velocity;
+
+    private int VillagerCount;
+
+	void Start ()
+    {
+        VillagerCount = 0;
 	}
 	
-	void Update () {
-	
+	void Update ()
+    {
+        if(Vector2.Distance(FindClosestVillager().transform.position, transform.position) < 56)
+        {
+            GoAfterVillager();
+        }
 	}
+
+    void AddVillagerPoint()
+    {
+
+    }
+
+    void GoAfterVillager()
+    {
+        float angle = Mathf.Atan2(
+            FindClosestVillager().transform.position.y - transform.position.y,
+            FindClosestVillager().transform.position.x - transform.position.x
+            ) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * WereWolf_Velocity;
+    }
+
+    GameObject FindClosestVillager()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Villager");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    void GoBack()
+    {
+
+    }
 }
