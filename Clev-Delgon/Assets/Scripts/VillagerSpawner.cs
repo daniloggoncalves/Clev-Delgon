298b58085035e31441274f0b494fd840dfcf3aa7
gabrieldf2000 <holaMG@Marias-iMac.local>
2015-10-11 03:25:54 +0900

    Spawn System

---
+++ Clev-Delgon/Assets/Scripts/VillagerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillagerSpawner : MonoBehaviour {
+
+	public GameObject villagerNPCPrefab;
+
+	//1
+	public float minSpawnTime = 0.75f; 
+	public float maxSpawnTime = 2f; 
+	
+	//2    
+	void Start () {
+		Invoke("SpawnVillager",minSpawnTime);
+	}
+	
+	//3
+	void SpawnVillager()
+	{
+
+		int i = GameObject.FindGameObjectsWithTag ("Villager").Length;
+		
+			if (i < 5)
+		{
+				Instantiate(villagerNPCPrefab, Vector3.zero, Quaternion.identity);
+		}
+
+		Invoke("SpawnVillager", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+}
