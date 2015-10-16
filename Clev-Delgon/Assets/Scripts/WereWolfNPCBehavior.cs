023ebe38071891ea90898ae317dd808d1e20b5b5
gabrieldf2000 <holaMG@Marias-iMac.local>
2015-10-17 01:49:34 +0900

    Pathfinder
    Pathfinding: finds path
    PathRequestManager: manages code for various IA

--- Clev-Delgon/Assets/Scripts/WereWolfNPCBehavior.cs
+++ Clev-Delgon/Assets/Scripts/WereWolfNPCBehavior.cs
@@ -3,66 +3,81 @@ using System.Collections;
 
 public class WereWolfNPCBehavior : MonoBehaviour {
 
-    public float WereWolf_Velocity;
+    public float WereWolf_VelocityMAX;
     static public int VillagerCountWerewolf;
     public GameObject VillagerSpawner;
     static public bool FollowingVillager;
+	public int WeightofVillagerforWerewolf;
 
-    public float wereWolf_Velocity;
+    private float wereWolf_VelocityACTUAL;
     private GameObject TemporaryFollowing;
     private GameObject Base;
     private GameObject Player;
     private bool PlayerAround;
     private bool ReturningBase;
 
+	int targetIndex;
+	Vector3[] path;
+
+
+
 
 	void Start ()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         Base = GameObject.FindGameObjectWithTag("Base");
-        GoToVillage();
+		VillagerSpawner = GameObject.FindGameObjectWithTag("VillagerSpawner");
+		// Go to Village
+		Follow (VillagerSpawner);
         VillagerCountWerewolf = 0;
 	}
+
 	
 	void Update ()
     {
 
-        wereWolf_Velocity = ((WereWolf_Velocity * (10 - VillagerCountWerewolf)) / 10);
+        wereWolf_VelocityACTUAL = WereWolf_VelocityMAX  - (VillagerCountWerewolf * (WeightofVillagerforWerewolf/10) );
 
+
+		//Return to the base after hunting 3 Villagers OR player around
         if (VillagerCountWerewolf >= 3 || PlayerAround == true)
         {
-            ReturnBase();
+            Follow(Base);
+			ReturningBase = true;
 
             if (Vector2.Distance(transform.position, Base.transform.position) <= 50)
-            {
-                ScoreScript.WerewolfScore = ScoreScript.WerewolfScore + VillagerCountWerewolf;
-                VillagerCountWerewolf = 0;
-                ReturningBase = false;
-                PlayerAround = false;
-                GoToVillage();
-                wereWolf_Velocity = WereWolf_Velocity;
-            }
+	            {
+	                ScoreScript.WerewolfScore = ScoreScript.WerewolfScore + VillagerCountWerewolf;
+	                VillagerCountWerewolf = 0;
+	                ReturningBase = false;
+	                PlayerAround = false;
+
+					//Go to village
+					Follow (VillagerSpawner);
+
+	                wereWolf_VelocityACTUAL = WereWolf_VelocityMAX;
+	            }
         }
 
         CheckForPlayer(Player);
 
+		// go to village or follow closest villager
         if (ReturningBase == false && PlayerAround == false)
         {
 
-            if (FollowingVillager == true)
-            {
-                FollowObject(TemporaryFollowing);
-            }
-            if (Vector2.Distance(FindClosestVillager().transform.position, transform.position) <100 && FollowingVillager == false)
-            {
-                TemporaryFollowing = FindClosestVillager();
-                FollowingVillager = true;
-            }
+			TemporaryFollowing = FindClosestVillager();
 
-            if (Vector2.Distance(FindClosestVillager().transform.position, transform.position) > 100 && FollowingVillager == false)
-            {
-                GoToVillage();
-            }
+            if (Vector2.Distance(TemporaryFollowing.transform.position, transform.position) < 100 && FollowingVillager == false)
+	            {
+					Follow(TemporaryFollowing);
+					FollowingVillager = true;
+	            }
+
+			if (Vector2.Distance(TemporaryFollowing.transform.position, transform.position) > 100 && FollowingVillager == false)
+	            {
+	                //Go to village
+					Follow(VillagerSpawner);
+	            }
         }
     }
 
@@ -72,36 +87,6 @@ public class WereWolfNPCBehavior : MonoBehaviour {
         {
             PlayerAround = true;
         }
-    }
-
-    void FollowObject(GameObject Object)
-    {
-        if (Object != null)
-        {
-            float angle = Mathf.Atan2(
-    Object.transform.position.y - transform.position.y,
-    Object.transform.position.x - transform.position.x
-    ) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0, 0, angle);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * wereWolf_Velocity;
-            FollowingVillager = true;
-        }
-        if (Object == null)
-        {
-            FollowingVillager = false;
-        }
-
-    }
-
-    void GoAfterVillager()
-    {
-        float angle = Mathf.Atan2(
-            FindClosestVillager().transform.position.y - transform.position.y,
-            FindClosestVillager().transform.position.x - transform.position.x
-            ) * Mathf.Rad2Deg;
-        transform.eulerAngles = new Vector3(0, 0, angle);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * wereWolf_Velocity;
-        FollowingVillager = true;
     }
 
     GameObject FindClosestVillager()
@@ -123,38 +108,32 @@ public class WereWolfNPCBehavior : MonoBehaviour {
         }
         return closest;
     }
-
-    void GoToVillage()
-    {
-        VillagerSpawner = GameObject.FindGameObjectWithTag("VillagerSpawner");
-
-
-        float angle = Mathf.Atan2(
-
-            VillagerSpawner.transform.position.y - transform.position.y,
-            VillagerSpawner.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
-
-
-        GetComponent<Rigidbody2D>().velocity = (new Vector2(Mathf.Cos(angle + 90), Mathf.Sin(angle + 90)) * wereWolf_Velocity);
-
-        transform.eulerAngles = new Vector3(0, 0, angle);
-
-    }
-
-    void ReturnBase()
-    {
-        Base = GameObject.FindGameObjectWithTag("Base");
-
-
-        float angle = Mathf.Atan2(
-        
-            Base.transform.position.y - transform.position.y,
-            Base.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
-
-
-        GetComponent<Rigidbody2D>().velocity = (new Vector2(Mathf.Cos(angle + 90), Mathf.Sin(angle + 90)) * wereWolf_Velocity);
-        transform.eulerAngles = new Vector3(0, 0, angle);
-        
-        ReturningBase = true;
-    }
+	
+	public void Follow(GameObject target)	{
+		PathRequestManager.RequestPath (transform.position, target.transform.position, OnPathFound);
+	}
+	
+	public void OnPathFound(Vector3[] newPath, bool pathSuccessful){
+		if (pathSuccessful) {
+			path = newPath;
+			StopCoroutine("FollowPath");
+			StartCoroutine("FollowPath");
+		}
+	}
+	
+	IEnumerator FollowPath() {
+		Vector3 currentWaypoint = path [0];
+		
+		while (true) {
+			if(transform.position == currentWaypoint){
+				targetIndex ++;
+				if (targetIndex >= path.Length){
+					yield break;
+				}
+				currentWaypoint = path[targetIndex];
+			}
+			transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, wereWolf_VelocityACTUAL);
+			yield return null;
+		}
+	}
 }
